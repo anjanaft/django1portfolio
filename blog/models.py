9ec47b675f78dcec8ac1dc49f215ac674383ea99
anjanaft <anjanaft13@gmail.com>
2019-10-18 11:49:18 +0530

    first commit

---
+++ blog/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+class Blog(models.Model):
+    title=models.CharField(max_length=200)
+    pub_date=models.DateTimeField()
+    body=models.TextField(max_length=200)
+    image=models.ImageField(upload_to='images/')
+
+    def __str__(self):
+        return self.title
+    def summary(self):
+            return self.body[:100]
+    def pub_date_pretty(self):
+            return self.pub_date.strftime('%b %e %Y' )
---
+++ jobs/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+# Create your models here.
+class Job(models.Model):
+    image = models.ImageField(upload_to='images/')
+    msg = models.CharField(max_length=200)
